a1c76e38e4e1c9d003fb01a8ef3ae8af741ab285
melrwa <orwamelkizedek@gmail.com>
2025-01-22 08:27:41 +0300

    complete lab

--- server/models.py
+++ server/models.py
@@ -12,6 +12,23 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError("Author name is required.")
+        
+        if db.session.query(Author).filter(Author.name==name).first():
+            raise ValueError("Author name must be unique")
+        return name
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if not phone_number or len(phone_number) !=10 or not phone_number.isdigit():
+            raise ValueError('Phone number must be exactly 10 digits.')
+        return phone_number
+
+    
+    
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -28,6 +45,32 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
+    @validates('content')
+    def validate_content(self, key, content):
+        if not content or len(content)<250:
+            raise ValueError('Post content must be at least 250 characters long.')
+        return content
+    
+    @validates('summary')
+    def validate_summary(self, key , summary):
+        if summary and len(summary)>250:
+            raise ValueError('Post summary must be 250 a maximum characters long.')
+        return summary
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in ['Fiction', 'Non-Fiction']:
+            raise ValueError("Category must be either 'Fiction' or 'Non-Fiction'.")
+        return category
+    
+    @validates('title')
+    def validate_title(self, key, title):
+        clickbait_phrases = ["Won't Believe", "Secret", "Top", "Gues"]
+        if not any(phrase in title for phrase in clickbait_phrases):
+            raise ValueError("Post Title must be sufficiently clickbait-y")
+        return title
+
+
 
 
     def __repr__(self):
